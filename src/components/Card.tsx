a97365105809963bb4d32996f21f47f1bc1b0f8f
oliv2673 <48830541+oliv2673@users.noreply.github.com>
2025-11-10 18:11:38 +0100

    fixed tailwind bug

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -14,10 +14,10 @@ interface CardProps {
 
 export default function Card({ pokemon, types }: CardProps) {
 	return (
-		<article className="relative rounded-xl shadow-lg p-6 bg-card-bg/100 backdrop-blur-sm transition-transform hover:scale-105">
-			<div className="space-y-2 mb-4">
-				<h1 className="font-play text-2xl font-bold capitalize text-white">{pokemon.name}</h1>
-				<h2 className="text-gray-300 text-sm">#{String(pokemon.id).padStart(4, "0")}</h2>
+		<article className="relative rounded-xl shadow-lg p-6 bg-card-bg/25 backdrop-blur-[2px] text-shadow-main">
+			<div className="space-y-2 mb-4 flex justify-end">
+
+				<h2 className="font-play text-2xl text-gray-300 text-white">#{String(pokemon.id).padStart(4, "0")}</h2>
 			</div>
 
 			<div className="relative aspect-square mb-4">
@@ -29,11 +29,14 @@ export default function Card({ pokemon, types }: CardProps) {
 					priority
 				/>
 			</div>
+			<div className="py-2">
+				<h2 className="font-play text-2xl font-bold capitalize text-white">{pokemon.name}</h2>
+			</div>
 
-			<div className="flex gap-2 flex-wrap">
+			<div className="flex gap-2 object-contain">
 				{types.map((type) => (
-					<div key={type.id} className="relative w-8 h-8">
-						<Image src={type.logo} alt={type.name} fill className="pixelated" unoptimized />
+					<div key={type.id} className="relative">
+						<Image src={type.logo} alt={type.name} width={64} height={28} className="pixelated object-contain" unoptimized />
 					</div>
 				))}
 			</div>
--- src/components/PokeGrid.tsx
+++ src/components/PokeGrid.tsx
@@ -27,7 +27,7 @@ export default async function PokeGrid() {
 			.filter((t): t is TypeInfo => t !== null);
 
 	return (
-		<div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+		<div className="grid gap-4 grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
 			{pokemons.map((pokemon) => (
 				<Card key={pokemon.id} pokemon={pokemon} types={makeTypeInfo(pokemon)} />
 			))}
